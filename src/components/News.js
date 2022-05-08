d66f7c7c6ac5c583fb16975ce8608b6a9f2cad12
BlitzGithub <k.ayushprogramming@gmail.com>
2022-05-08 10:39:38 +0530

    final changes

--- src/components/News.js
+++ src/components/News.js
@@ -1,226 +1,226 @@
-// import React, { useEffect, useState } from "react";
-// import NewsItem from "./NewsItem";
-// import "./all.css";
-// import Spinner from "./Spinner";
-// import PropTypes from "prop-types";
-// import InfiniteScroll from "react-infinite-scroll-component";
-
-// const News = (props) => {
-//   const [articles, setArticles] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [page, setPage] = useState(1);
-//   const [totalResults, setTotalResults] = useState(0);
-
-//   const capitalize = (string) => {
-//     return string.charAt(0).toUpperCase() + string.slice(1);
-//   };
-
-//   useEffect(() => {
-//     document.title = `NewzX-${
-//       props.category === "general"
-//         ? "Global News App"
-//         : capitalize(props.category)
-//     }`;
-//     updateNews();
-//     // eslint-disable-next-line
-//   }, []);
-
-//   const updateNews = async () => {
-//     props.setProgress(10);
-//     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page}&pageSize=${props.pageSize}`;
-//     setLoading(true);
-//     let data = await fetch(url);
-//     props.setProgress(30);
-//     let parseddata = await data.json();
-//     props.setProgress(70);
-//     setArticles(parseddata.articles);
-//     setTotalResults(parseddata.totalResults);
-//     setLoading(false);
-//     props.setProgress(100);
-//   };
-
-//   const fetchMoreData = async () => {
-//     const url = `https://newsapi.org/v2/top-headlines?country=${
-//       props.country
-//     }&category=${props.category}&apiKey=${props.apikey}&page=${
-//       page + 1
-//     }&pageSize=${props.pageSize}`;
-//     setPage(page + 1);
-//     let data = await fetch(url);
-//     let parseddata = await data.json();
-
-//     setArticles(articles.concat(parseddata.articles));
-//     setTotalResults(parseddata.totalResults);
-//   };
-
-//   return (
-//     <>
-//       <h2 className="header"> NewzX - Global News App</h2>
-//       <h2 className="header"> {capitalize(props.category)} News</h2>
-//       {loading && <Spinner />}
-//       <InfiniteScroll
-//         dataLength={articles.length}
-//         next={fetchMoreData}
-//         hasMore={articles.length !== totalResults}
-//         loader={<Spinner />}
-//       >
-//         <div className="container">
-//           <div className="row">
-//             {articles.map((element) => {
-//               return (
-//                 <div className="col-md-4" key={element.url}>
-//                   <NewsItem
-//                     title={element.title ? element.title.slice(0, 45) : ""}
-//                     description={
-//                       element.description
-//                         ? element.description.slice(0, 85)
-//                         : ""
-//                     }
-//                     imgurl={
-//                       element.urlToImage
-//                         ? element.urlToImage
-//                         : "https://logodix.com/logo/570888.png"
-//                     }
-//                     newsurl={element.url}
-//                     author={element.author ? element.author : "Anonymous"}
-//                     date={element.publishedAt}
-//                     source={element.source.name}
-//                   />
-//                 </div>
-//               );
-//             })}
-//           </div>
-//         </div>
-//       </InfiniteScroll>
-//     </>
-//   );
-// };
-
-// News.defaultProps = {
-//   country: "in",
-//   pageSize: 8,
-//   category: "general",
-// };
-// News.propTypes = {
-//   country: PropTypes.string,
-//   pageSize: PropTypes.number,
-//   category: PropTypes.string,
-// };
-
-// export default News;
-
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
-import Spinner from "./Spinner";
 import "./all.css";
+import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-  static defaultProps = {
-    country: "in",
-    pageSize: 8,
-    category: "general",
-  };
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
-  capitalizeFirstLetter = (string) => {
+  const capitalize = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      loading: true,
-      page: 1,
-      totalResults: 0,
-    };
+
+  useEffect(() => {
     document.title = `NewzX-${
-      this.props.category === "general"
+      props.category === "general"
         ? "Global News App"
-        : this.capitalizeFirstLetter(this.props.category)
+        : capitalize(props.category)
     }`;
-  }
+    updateNews();
+    // eslint-disable-next-line
+  }, []);
 
-  async updateNews() {
-    this.props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+  const updateNews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(url);
-    this.props.setProgress(30);
-    let parsedData = await data.json();
-    this.props.setProgress(70);
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false,
-    });
-    this.props.setProgress(100);
-  }
-  async componentDidMount() {
-    this.updateNews();
-  }
-
-  fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      totalResults: parsedData.totalResults,
-    });
+    props.setProgress(30);
+    let parseddata = await data.json();
+    props.setProgress(70);
+    setArticles(parseddata.articles);
+    setTotalResults(parseddata.totalResults);
+    setLoading(false);
+    props.setProgress(100);
   };
 
-  render() {
-    return (
-      <>
-        <h2 className="header"> NewzX - Global News App</h2>
-        <h2 className="header">
-          {this.capitalizeFirstLetter(this.props.category)} News
-        </h2>
-        {this.state.loading && <Spinner />}
-        <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalResults}
-          loader={<Spinner />}
-        >
-          <div className="container">
-            <div className="row">
-              {this.state.articles.map((element) => {
-                return (
-                  <div className="col-md-4" key={element.url}>
-                    <NewsItem
-                      title={element.title ? element.title.slice(0, 45) : ""}
-                      description={
-                        element.description
-                          ? element.description.slice(0, 85)
-                          : ""
-                      }
-                      imgurl={
-                        element.urlToImage
-                          ? element.urlToImage
-                          : "https://logodix.com/logo/570946.png"
-                      }
-                      newsurl={element.url}
-                      author={element.author ? element.author : "Anonymous"}
-                      date={element.publishedAt}
-                      source={element.source.name}
-                    />
-                  </div>
-                );
-              })}
-            </div>
+  const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${
+      props.country
+    }&category=${props.category}&apiKey=${props.apikey}&page=${
+      page + 1
+    }&pageSize=${props.pageSize}`;
+    setPage(page + 1);
+    let data = await fetch(url);
+    let parseddata = await data.json();
+
+    setArticles(articles.concat(parseddata.articles));
+    setTotalResults(parseddata.totalResults);
+  };
+
+  return (
+    <>
+      <h2 className="header"> NewzX - Global News App</h2>
+      <h2 className="header"> {capitalize(props.category)} News</h2>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 85)
+                        : ""
+                    }
+                    imgurl={
+                      element.urlToImage
+                        ? element.urlToImage
+                        : "https://logodix.com/logo/570946.png"
+                    }
+                    newsurl={element.url}
+                    author={element.author ? element.author : "Anonymous"}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
           </div>
-        </InfiniteScroll>
-      </>
-    );
-  }
-}
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+};
+
+News.defaultProps = {
+  country: "in",
+  pageSize: 8,
+  category: "general",
+};
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
 
 export default News;
+
+// import React, { Component } from "react";
+// import NewsItem from "./NewsItem";
+// import Spinner from "./Spinner";
+// import "./all.css";
+// import PropTypes from "prop-types";
+// import InfiniteScroll from "react-infinite-scroll-component";
+
+// export class News extends Component {
+//   static defaultProps = {
+//     country: "in",
+//     pageSize: 8,
+//     category: "general",
+//   };
+
+//   static propTypes = {
+//     country: PropTypes.string,
+//     pageSize: PropTypes.number,
+//     category: PropTypes.string,
+//   };
+//   capitalizeFirstLetter = (string) => {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+//   };
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       articles: [],
+//       loading: true,
+//       page: 1,
+//       totalResults: 0,
+//     };
+//     document.title = `NewzX-${
+//       this.props.category === "general"
+//         ? "Global News App"
+//         : this.capitalizeFirstLetter(this.props.category)
+//     }`;
+//   }
+
+//   async updateNews() {
+//     this.props.setProgress(10);
+//     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+//     this.setState({ loading: true });
+//     let data = await fetch(url);
+//     this.props.setProgress(30);
+//     let parsedData = await data.json();
+//     this.props.setProgress(70);
+//     this.setState({
+//       articles: parsedData.articles,
+//       totalResults: parsedData.totalResults,
+//       loading: false,
+//     });
+//     this.props.setProgress(100);
+//   }
+//   async componentDidMount() {
+//     this.updateNews();
+//   }
+
+//   fetchMoreData = async () => {
+//     this.setState({ page: this.state.page + 1 });
+//     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+//     let data = await fetch(url);
+//     let parsedData = await data.json();
+//     this.setState({
+//       articles: this.state.articles.concat(parsedData.articles),
+//       totalResults: parsedData.totalResults,
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <h2 className="header"> NewzX - Global News App</h2>
+//         <h2 className="header">
+//           {this.capitalizeFirstLetter(this.props.category)} News
+//         </h2>
+//         {this.state.loading && <Spinner />}
+//         <InfiniteScroll
+//           dataLength={this.state.articles.length}
+//           next={this.fetchMoreData}
+//           hasMore={this.state.articles.length !== this.state.totalResults}
+//           loader={<Spinner />}
+//         >
+//           <div className="container">
+//             <div className="row">
+//               {this.state.articles.map((element) => {
+//                 return (
+//                   <div className="col-md-4" key={element.url}>
+//                     <NewsItem
+//                       title={element.title ? element.title.slice(0, 45) : ""}
+//                       description={
+//                         element.description
+//                           ? element.description.slice(0, 85)
+//                           : ""
+//                       }
+//                       imgurl={
+//                         element.urlToImage
+//                           ? element.urlToImage
+//                           : "https://logodix.com/logo/570946.png"
+//                       }
+//                       newsurl={element.url}
+//                       author={element.author ? element.author : "Anonymous"}
+//                       date={element.publishedAt}
+//                       source={element.source.name}
+//                     />
+//                   </div>
+//                 );
+//               })}
+//             </div>
+//           </div>
+//         </InfiniteScroll>
+//       </>
+//     );
+//   }
+// }
+
+// export default News;
